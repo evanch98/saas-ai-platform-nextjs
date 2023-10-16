734edebd509a7dbb25db905e3746251d4151f426
Kyaw Thu <evanch98@gmail.com>
2023-10-16 11:51:14 +0800

    test the shadcn ui button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p className="text-6xl text-green-500">Hello AI SaaS</p>;
+  return <Button variant="destructive">Click Me</Button>;
 }
