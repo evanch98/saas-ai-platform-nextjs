bd959d55a3e3fb64d9b970f4e1fa1b6f02cd6e0d
Kyaw Thu <evanch98@gmail.com>
2023-10-17 12:00:20 +0800

    update the Heading component

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,3 +1,19 @@
-export const Heading = () => {
+import { LucideIcon } from "lucide-react";
+
+interface HeadingProps {
+  title: string;
+  description: string;
+  icon: LucideIcon;
+  iconColor?: string;
+  bgColor?: string;
+}
+
+export const Heading = ({
+  title,
+  description,
+  icon,
+  iconColor,
+  bgColor,
+}: HeadingProps) => {
   return <div>Heading Component</div>;
 };
