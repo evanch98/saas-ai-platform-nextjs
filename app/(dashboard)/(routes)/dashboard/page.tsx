ae69ae598aa1454c593f066d98477bf55b851d6d
Kyaw Thu <evanch98@gmail.com>
2023-10-17 11:41:48 +0800

    update the DashboardPage

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
 const DashboardPage = () => {
-  return <div>Dashboard Page (Protected)</div>;
+  return (
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4"></div>
+    </div>
+  );
 };
 
 export default DashboardPage;
