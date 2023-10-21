fc44b9a7449995e55a5cf0c59fcba2c617ae49f0
Kyaw Thu <evanch98@gmail.com>
2023-10-21 12:26:07 +0800

    update the ProModal

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -6,10 +6,12 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 export const ProModal = () => {
+  const proModal = useProModal();
   return (
-    <Dialog open>
+    <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
