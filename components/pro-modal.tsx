318eec69cfad5a0a974bc8627311426814f920e2
Kyaw Thu <evanch98@gmail.com>
2023-10-21 13:38:11 +0800

    update the ProModal

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -18,7 +18,9 @@ export const ProModal = () => {
           <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
             <div className="flex items-center gap-x-2 font-bold py-1">
               Upgrade to Genius
-              <Badge className="uppercase text-sm py-1">pro</Badge>
+              <Badge variant="premium" className="uppercase text-sm py-1">
+                pro
+              </Badge>
             </div>
           </DialogTitle>
         </DialogHeader>
