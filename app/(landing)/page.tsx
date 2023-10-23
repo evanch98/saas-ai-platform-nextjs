d4b4ca1bcdaa3366457bc6cd2137d4bae6012e01
Kyaw Thu <evanch98@gmail.com>
2023-10-23 17:10:23 +0800

    update the LandingPage

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,11 @@
+import { LandingNavbar } from "@/components/landing-navbar";
+
 const LandingPage = () => {
-  return <div className="h-full">
-    <LandingNavbar />
-  </div>;
+  return (
+    <div className="h-full">
+      <LandingNavbar />
+    </div>
+  );
 };
 
 export default LandingPage;
